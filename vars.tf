b12c1b201ea01bad85d4b98cc1eb4e7740ba321b
backend_dev1 <backend_dev1@gmail.com>
2022-07-21 18:10:31 +0530

    Updated

--- vars.tf
+++ vars.tf
@@ -2,9 +2,9 @@ variable "region" {
     default = "us-east-1" 
 }
 
-variable "cidr_block" {
+/* variable "cidr_block" {
     default = "10.0.0.0/16"
-}
+} */
  
 variable "instance_tenancy" {
     default = "default"
