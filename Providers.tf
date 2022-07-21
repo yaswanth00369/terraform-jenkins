7f79cba34f06b576e3533ae79bbfc5e80cfccc0a
backend_dev1 <backend_dev1@gmail.com>
2022-07-21 17:45:55 +0530

    Updated

--- Providers.tf
+++ Providers.tf
@@ -2,11 +2,11 @@ provider "aws" {
     region = var.region
 }
 
-terraform {
+/* terraform {
   backend "s3" {
     bucket = "jenkins-tfstate-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "Game-Scores"
   }
-} 
+}  */
