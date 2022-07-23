8007ae0d400a9337a185d210d2db8c18f9ea7eb7
backend_dev1 <backend_dev1@gmail.com>
2022-07-23 13:30:56 +0530

    Update

--- Providers.tf
+++ Providers.tf
@@ -2,11 +2,11 @@ provider "aws" {
     region = var.region
 }
 
-terraform {
+/* terraform {
   backend "s3" {
     bucket = "jenkins-yaml-tfstate-bucket"
     key    = "terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "jenkins-yaml-table"
   }
-} 
+}  */
